17dac6b6ab0cb2418ebfdd40b6d942dd89e19d29
Efendi <tanefendi.97@gmail.com>
2022-12-15 10:24:12 +0700

    add button function

--- main.js
+++ main.js
@@ -1,125 +1,179 @@
-function add(a,b){
-    return a+b;
-}
-function subtract(a,b){
-    return a-b;
-}
-function multiply(a,b){
-    return a*b;
-}
-function divide(a,b){
-    return a/b;
-}
+let a;
+let b;
 let operator;
-function operate(operator,a,b){
-    if(operator===add){
-        return a+b;
-    }
-    else if(operator===subtract){
-        return a-b;
-    }
-    else if(operator===multiply){
-        return a*b;
-    }
-    else if(operator===divide){
-        return a/b;
-    }
-}
+let num='';
+const operand=[0];
 
-const numBasket=[];
-const secondNumBasket=[];
 let screen = document.querySelector('.screen');
 screen.textContent = 0;
 
-// THIS ONLY DELETE 1 DIGIT (BACKSPACE BUTTON IDEA)
-// let buttonClear=document.querySelector('.clear');
-// buttonClear.addEventListener('click',()=>{
-//     numBasket.pop()
-//     screen.textContent=Number(numBasket.join(''));
-// })
-
-let buttonClear=document.querySelector('.clear');
-buttonClear.addEventListener('click',()=>{
-    while(numBasket.length!==0){
-        numBasket.pop();
+let clearButton=document.querySelector('.clear');
+clearButton.addEventListener('click',allClear)
+function allClear(){
+    while(operand.length!==0){
+        operand.pop();
     }
-    secondNumBasket.pop();
-    screen.textContent=Number(numBasket.join(''));
+    num='';
+    screen.textContent=0;
+}
+let numberButton = document.querySelectorAll('.number');
+for(let i=0;i<numberButton.length;i++){
+    numberButton[i].addEventListener('click',()=>{
+        num+=numberButton[i].textContent;
+        screen.textContent=num;
+    })
+}
+let commaButton = document.querySelector('.comma');
+commaButton.addEventListener('click',()=>{
+    if(num.includes('.')===false){
+        num+='.';
+        screen.textContent=num;
+    }
 })
 
-let buttonOne = document.querySelector('.one')
-buttonOne.addEventListener('click',()=>{
-    numBasket.push(1);
-    screen.textContent=Number(numBasket.join(''));
-})
-let buttonTwo = document.querySelector('.two')
-buttonTwo.addEventListener('click',()=>{
-    numBasket.push(2);
-    screen.textContent=Number(numBasket.join(''));
-})
-let buttonThree = document.querySelector('.three')
-buttonThree.addEventListener('click',()=>{
-    numBasket.push(3);
-    screen.textContent=Number(numBasket.join(''));
-})
-let buttonFour = document.querySelector('.four')
-buttonFour.addEventListener('click',()=>{
-    numBasket.push(4);
-    screen.textContent=Number(numBasket.join(''));
-})
-let buttonFive = document.querySelector('.five')
-buttonFive.addEventListener('click',()=>{
-    numBasket.push(5);
-    screen.textContent=Number(numBasket.join(''));
-})
-let buttonSix = document.querySelector('.six')
-buttonSix.addEventListener('click',()=>{
-    numBasket.push(6);
-    screen.textContent=Number(numBasket.join(''));
-})
-let buttonSeven = document.querySelector('.seven')
-buttonSeven.addEventListener('click',()=>{
-    numBasket.push(7);
-    screen.textContent=Number(numBasket.join(''));
-})
-let buttonEight = document.querySelector('.eight')
-buttonEight.addEventListener('click',()=>{
-    numBasket.push(8);
-    screen.textContent=Number(numBasket.join(''));
-})
-let buttonNine = document.querySelector('.nine')
-buttonNine.addEventListener('click',()=>{
-    numBasket.push(9);
-    screen.textContent=Number(numBasket.join(''));
-})
-let buttonZero = document.querySelector('.zero')
-buttonZero.addEventListener('click',()=>{
-    numBasket.push(0);
-    screen.textContent=Number(numBasket.join(''));
-})
-let buttonComma = document.querySelector('.comma')
-buttonComma.addEventListener('click',()=>{
-    numBasket.push('.');
-    screen.textContent=Number(numBasket.join(''));
-})
 let buttonAdd = document.querySelector('.add');
-buttonAdd.addEventListener('click',()=>{
-    operator=add;
-    secondNumBasket.push(Number(numBasket.join('')));
-    while(numBasket.length!==0){
-        numBasket.pop();
+buttonAdd.addEventListener('click',addButton);
+function addButton(){
+    if(operator==='subtract'){
+        subtractButton();
     }
-})
+    else if(operator==='multiply'){
+        multiplyButton();
+    }
+    else if(operator==='divide'){
+        divideButton();
+    }
+    else{
+        if(Number(num)!==0){
+            operand.push(Number(num));
+        }
+        num='';
+        screen.textContent=num;
+        let total=operand[0]
+        for(let i=1;i<operand.length;i++){
+            total+=operand[i];
+        };
+        while(operand.length>0){
+            operand.pop();
+        };
+        operand.push(total);
+        screen.textContent=operand[0];
+    }
+    operator='add';
+}
+
+let buttonSubtract = document.querySelector('.subtract');
+buttonSubtract.addEventListener('click',subtractButton)
+function subtractButton(){
+    if(operator==='add'){
+        addButton();
+    }
+    else if(operator==='multiply'){
+        multiplyButton();
+    }
+    else if(operator==='divide'){
+        divideButton();
+    }
+    else{
+        operator=undefined;
+        if(Number(num)!==0){
+            operand.push(Number(num));
+        }
+        num='';
+        screen.textContent=num;
+        let total=operand[0];
+        for(let i=1;i<operand.length;i++){
+            total-=operand[i];
+        };
+        while(operand.length>0){
+            operand.pop();
+        };
+        operand.push(total);
+        screen.textContent=operand[0];
+    }
+    operator='subtract';
+}
+
+let buttonMultiply = document.querySelector('.multiply');
+buttonMultiply.addEventListener('click',multiplyButton);
+function multiplyButton(){
+    if(operator==='add'){
+        addButton();
+    }
+    else if(operator==='subtract'){
+        subtractButton();
+    }
+    else if(operator==='divide'){
+        divideButton();
+    }
+    else{
+        operator=undefined;
+        if(Number(num)!==0){
+            operand.push(Number(num));
+        }
+        num='';
+        screen.textContent=num;
+        if(operand[0]===0){
+            operand[0]=1;
+        }
+        let total=operand[0];
+        for(let i=1;i<operand.length;i++){
+            total*=operand[i];
+        };
+        while(operand.length>0){
+            operand.pop();
+        };
+        operand.push(total);
+        screen.textContent=operand[0];
+    }
+    operator='multiply';
+}
+let buttonDivide = document.querySelector('.divide');
+buttonDivide.addEventListener('click',divideButton);
+function divideButton(){
+    if(operator==='add'){
+        addButton();
+    }
+    else if(operator==='subtract'){
+        subtractButton();
+    }
+    else if(operator==='multiply'){
+        multiplyButton();
+    }
+    else{
+        operator=undefined;
+        if(Number(num)!==0){
+            operand.push(Number(num));
+        }
+        num='';
+        screen.textContent=num;
+        if(operand[0]===0){
+            operand[0]=1;
+        }
+        let total=operand[0];
+        for(let i=1;i<operand.length;i++){
+            total/=operand[i];
+        };
+        while(operand.length>0){
+            operand.pop();
+        };
+        operand.push(total);
+        screen.textContent=operand[0];
+    }
+    operator='divide';
+}
 let buttonResult = document.querySelector('.result');
 buttonResult.addEventListener('click',()=>{
-    let result = operate(operator,Number(secondNumBasket.join('')),Number(numBasket.join('')));
-    screen.textContent=result;
-    while(numBasket.length!==0){
-        numBasket.pop();
+    if(operator==='add'){
+        addButton();
     }
-    secondNumBasket.pop();
-    
+    else if(operator==='subtract'){
+        subtractButton();
+    }
+    else if(operator==='multiply'){
+        multiplyButton();
+    }
+    else if(operator==='divide'){
+        divideButton();
+    }
 })
-
-
-
